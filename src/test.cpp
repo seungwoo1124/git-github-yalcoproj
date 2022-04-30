4787b3fc1e5a900e25771c13eb1f008f6312bd91
정승우 <seungwoo1124@kookmin.ac.kr>
2022-05-01 00:33:12 +0900

    time test

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-#include "testheader.h"
-#include "ncurses.h"
-
+#include <time.h>
 using namespace std;
 
 int main()
 {
-    test();
-    cout << "hello World" << endl;
+    long prevTick = clock();
+    long sumTick = 0;
+    double prevsec = 0.0;
+    while (sumTick < 10000000)
+    {
+        long curTick = clock();
+        long deltaTick = curTick - prevTick;
+        double realsec = (double)(curTick / CLOCKS_PER_SEC);
+        sumTick += deltaTick;
+        if (realsec != prevsec)
+        {
+            cout << realsec << endl;
+            prevsec = realsec;
+        }
+        prevTick = curTick;
+
+    }
 }
--- src/testheader.h
+++ src/testheader.h
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "ncurses.h"
+#include <unistd.h>
+
 using namespace std;
+
+int func1()
+{
+    initscr();
+    mvprintw(1, 1, "Hello World");
+    refresh();
+    sleep(2);
+    endwin();
+    return 0;
+}
 
 void test()
 {
